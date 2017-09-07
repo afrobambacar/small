dcaf25fd0d6f7a8de2725b1196e1c7d67d89cc36
Jaecheol Lee <jclee.dev@gmail.com>
2017-09-07 19:46:45 +0900

    dd

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/hello', function (req, res) {
+  res.send('world');
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
