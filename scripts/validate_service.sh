084ab82a20a1013979efdc5421c2f3e548f0a0bc
Jaecheol Lee <jclee.dev@gmail.com>
2017-09-08 16:36:30 +0900

    waiting pm2 process

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-result=$(curl -s http://localhost:3000/hello)
-if [[ "$result" =~ "world" ]]; then
-  exit 0
-else
+counter=1
+retries=5
+interval=3
+
+function healthcheck {
+  result=$(curl -s http://localhost:3000/hello)
+}
+
+while [ $counter -lt $retries ]
+do
+  healthcheck
+  if [[ "$result" == "world" ]]; then
+    echo "connected"
+    exit 0
+  else
+    echo "disconnected $counter"
+    sleep $interval
+    ((counter++))
+  fi
+done
+  echo "timeouted"
   exit 1
-fi
