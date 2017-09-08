2fe198416f3fc0efbea12dda35a267dc1bff71db
Jaecheol Lee <jclee.dev@gmail.com>
2017-09-08 19:22:50 +0900

    validate 200 status code

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -5,17 +5,17 @@ retries=5
 interval=3
 
 function healthcheck {
-  result=$(curl -s http://localhost/hello)
+  status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost)
 }
 
 while [ $counter -lt $retries ]
 do
   healthcheck
-  if [[ "$result" -eq "world" ]]; then
-    echo "connected"
+  if [ $status -eq 200 ]; then
+    echo "connected $status"
     exit 0
   else
-    echo "disconnected $counter"
+    echo "disconnected $counter $status"
     sleep $interval
     ((counter++))
   fi
