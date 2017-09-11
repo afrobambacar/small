38858681cbed62a691da4c0c8b4568fdbd6bc472
Jaecheol Lee <jclee.dev@gmail.com>
2017-09-11 18:39:45 +0900

    health check

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
+# passing condition is that healthy count equal to healthy_threshold
+host=$1
+healthy=1
+unhealthy=1
 
-counter=1
-retries=5
+retries=20
+healthy_threshold=5
+unhealthy_threshold=5
 interval=3
 
 function healthcheck {
-  status=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:3000)
+  status=$(curl -s -o /dev/null -w "%{http_code}" $host)
 }
 
-while [ $counter -lt $retries ]
+function healthy_log {
+  echo "[$1] connected: healthy count ($2/$3) (left over $4)"
+}
+
+function unhealthy_log {
+  echo "[$1] disconnected: unhealthy count ($2/$3) (left over $4)"
+}
+
+while [ $retries -gt 0 ]
 do
   healthcheck
+
   if [ $status -eq 200 ]; then
-    echo "connected $status"
-    exit 0
+    if [ $healthy -eq $healthy_threshold ]; then
+      healthy_log $status $healthy $healthy_threshold $retries
+
+      exit 0
+    else
+      healthy_log $status $healthy $healthy_threshold $retries
+
+      ((healthy++))
+      ((retries--))
+      sleep $interval
+    fi
   else
-    echo "disconnected $counter $status"
+    unhealthy_log $status $unhealthy $unhealthy_threshold $retries
+
+    ((unhealthy++))
+    ((retries--))
     sleep $interval
-    ((counter++))
   fi
 done
   echo "timeouted"
